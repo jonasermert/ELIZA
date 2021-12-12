92f6377849004640497cda5a2a03e79c84b278e2
Jonas Ermert <ermert.jonas@yahoo.de>
2021-12-13 00:41:52 +0100

    Project ELIZA

---
+++ src/com/jonasermert/Main.java
@@ -0,0 +1,24 @@
+package com.jonasermert;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println(Doctor.intro());
+        var userInput = "";
+        userInput = scanner.nextLine();
+
+        while(!userInput.equalsIgnoreCase("quit")){
+            userInput = scanner.nextLine();
+            String response = Doctor.response(userInput);
+            System.out.println(response);
+        }
+
+        scanner.close();
+
+    }
+}
